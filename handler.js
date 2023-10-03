47b1725d3b43e44a0bdb1966a62501b352da6e0f
Anton Sheviardou <anton_sheviardou@epam.com>
2023-10-03 12:17:07 +0300

    init serverless lambda

---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+var message = 'Hello World'  
+  
+  console.log(event);
+
+  const name = event.queryStringParameters && event.queryStringParameters.Name;
+  if (name !== null) {
+    message = 'Hi ' + name
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: message,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
